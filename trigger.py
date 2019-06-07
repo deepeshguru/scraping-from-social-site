0751dd06518c996824b81e2a80ade1c297063890
deepeshguru <39788252+deepeshguru@users.noreply.github.com>
2019-06-07 13:08:47 +0530

    Add files via upload
    it trigger the code

---
+++ trigger.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  6 16:30:18 2019
+
+@author: Deepesh Agrawal
+"""
+
+import os
+import datetime
+while(1):
+    if datetime.datetime.now().time() == datetime.time(12,0,0,0):
+        os.system('python combined_insta_twitter.py')
+    
+
